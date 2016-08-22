89b918e89f28bf31e6ac571fdb6f6db2fdbbf628
Daniel Stokoe <daniel.stokoe@gmail.com>
2016-08-22 17:28:00 +0100

    Add files via upload

---
+++ coursework/coursework4.js
@@ -0,0 +1,11 @@
+function background(bg)
+{
+	if (document.body.style.backgroundColor == bg)
+	{
+		document.body.style.backgroundColor = "White";
+	}
+	else
+	{
+		document.body.style.backgroundColor = bg;
+	}
+}
